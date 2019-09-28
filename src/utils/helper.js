919b370e84cc154206c288ef1a203e1fef00ce73
jsantos <johnmichaelubas.santos@gmail.com>
2019-09-29 03:27:52 +0800

    oath unionbank

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -9,4 +9,7 @@ export const helper = {
   chance,
   cap,
   runFixtures,
+  unionbankOath: () => {
+    window.location.href = `${process.env.REACT_APP_UNIONBANK_BASE_URL}/convergent/v1/oauth2/authorize?response_type=code&client_id=${process.env.REACT_APP_UNIONBANK_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_UNIONBANK_REDIRECT_URI}&scope=payments`;
+  },
 };
