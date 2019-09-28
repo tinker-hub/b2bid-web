ee2ad0107cb1a24e27d8f3c3a010da2add29cb6c
jsantos <jsantos@ISBXMacBookPro2013.local>
2019-09-29 01:46:57 +0800

    Update

--- src/hooks/useUnionbankApi.js
+++ src/hooks/useUnionbankApi.js
@@ -7,4 +7,48 @@ const { useApi: useUnionbankApi } = createApi(
   }),
 );
 
+export const useUnionbankGetAccessToken = code => {
+  if (!code) {
+    throw new Error('No code specified');
+  }
+  const api = useUnionbankApi();
+  const token = api.convergent.v1.oauth2.token.post({
+    grant_type: 'authorization_code',
+    client_id: process.env.REACT_APP_UNIONBANK_CLIENT_ID,
+    client_secret: process.env.REACT_APP_UNIONBANK_CLIENT_SECRET,
+    code,
+    // Own redirect?
+    redirect_uri: 'https://api-uat.unionbankph.com/ubp/uat/v1/redirect',
+  });
+  return token;
+};
+
+export const useUnionbankFundTransfer = () => {
+  const api = useUnionbankApi();
+  const transferRequest = api.partners.v1.transfers.single.post({
+    senderTransferId: 'TRANSFER-0001',
+    transferRequestDate: '2017-10-10T12:11:50Z',
+    accountNo: '0001',
+    amount: {
+      currency: 'PHP',
+      value: '100',
+    },
+    remarks: 'Transfer remarks',
+    particualtrs: 'Transfer particulars',
+    info: [
+      {
+        index: 1,
+        name: 'Recipient',
+        value: 'Juan Dela Cruz',
+      },
+      {
+        index: 2,
+        name: 'Message',
+        value: 'Happy Investment',
+      },
+    ],
+  });
+  return transferRequest;
+};
+
 export { useUnionbankApi };
